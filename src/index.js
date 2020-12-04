8673b2e8ec844cda47e32878f0d4cd6e6dc8d4b9
LuisDFJ <luis.fuju@outlook.com>
2020-12-03 23:51:04 -0600

    Serial communication available

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const sPort = new SerialPort('/dev/ttyACM1');
+
 const app = express();
-const port = 3000;
+const port = 3333;
+
 
 var LED1 = 1;
 var LED2 = 1;
@@ -15,6 +20,8 @@ var V1 = 0;
 var V2 = 0;
 var PD1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var PD2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+const parser = sPort.pipe(new Readline({ delimiter: '\r\n'}));
 
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -38,7 +45,12 @@ app.post("/output", (req,res) => {
 });
 
 app.post("/input", (req,res) => {
-    console.log(req.body.data);
+    sPort.write("Q");
+    sPort.write("q");
+    sPort.write("c");
+    sPort.write("C");
+    sPort.write("V");
+    sPort.write("v");    
     C1 = Math.random() * 300;
     C2 = Math.random() * 300;
     V1 = Math.random() * 5;
@@ -51,11 +63,18 @@ app.post("/input", (req,res) => {
     PD2.push(P2);
     if (req.body.data === 'OK') {
         res.json({Current1: C1, Current2: C2, Power1: P1, Power2: P2, Voltage1: V1, Voltage2: V2, PowerData1: PD1, PowerData2: PD2});
+    } else {
+        console.log('ERROR at /input');
     }
 });
 
 app.use(express.static(path.join(__dirname,'/public')));
 
 const server = app.listen(port, () => {
-    console.log("Listo");
+    console.log("Listo");  
 });
+
+parser.on('data', (data) => {
+    console.log(data);
+});
+
